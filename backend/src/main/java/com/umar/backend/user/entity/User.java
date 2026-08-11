4fca29d2d028f11f54bdc21c1a7d1ac7a189eec6
Umar Firoz <f.umar@op.iitg.ac.in>
2026-08-11 10:19:10 +0530

    coded user repo and entity

--- backend/src/main/java/com/umar/backend/user/entity/User.java
+++ backend/src/main/java/com/umar/backend/user/entity/User.java
@@ -1,4 +1,28 @@
 package com.umar.backend.user.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String username;
+
+    @Size(min = 6, max = 20)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
 }
--- backend/src/main/java/com/umar/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/umar/backend/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.umar.backend.user.repository;
 
-public interface UserRepository {
+import com.umar.backend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
 }
